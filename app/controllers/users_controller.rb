59d3837caf13f602937c14a0156ebfe2244fe13b
Jake <jasilver@email.wm.edu>
2013-12-03 23:01:01 -0500

    working on models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,171 @@
 class UsersController < ApplicationController
+  skip_before_filter :signed_in_user, only: [:new,:create]
+
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user }
+      end
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
   def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+
+  def create
+    @user = User.new(params[:user])
+    @user.high_score = 0
+    @user.total_score = 0
+
+
+    if @user.save
+      respond_to do |format|
+        format.json { render json: "{'message':'user created'}"}
+      end
+    else
+      respond_to do |format|
+        format.json { render json: "{'message':'ERROR'}"}
+      end
+
+    end
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def user_details
+    details=Hash.new
+    details['vote']="no"
+    details['kill']="no"
+    details['total_score'] = current_user.total_score
+    details['high_score'] = current_user.high_score
+    details['username'] = current_user.email.split("@")[0]
+    details['rank']=User.order('total_score').all.index(current_user)
+    if (!Game.last.nil?) and (Game.last.game_state != "ended")
+      details['isgame'] = "playing"
+      @player = Player.find_by_user_id(current_user.id)
+      if (@player.isDead == "true")
+        details['status'] = "Dead"
+      else
+        details['status'] = "Alive: " + Player.find_by_user_id(current_user.id).alignment.upcase
+        if Player.find_by_user_id(current_user.id).alignment == "werewolf"
+          details['kill']="yes"
+        else
+          details['vote']="yes"
+        end
+      end
+      details['game_score'] = Player.find_by_user_id(current_user.id).score
+      details['alive'] = Player.find_by_user_id(current_user.id).isDead
+      details['werewolf']=0
+      details['townsperson']=0
+      Player.all.each do |player|
+        if player.isDead == "false"
+          details[player.alignment] = details[player.alignment] + 1
+        end
+      end
+      if (((Time.now - Game.find(@player.game_ID).created_at) % (120*Game.find(@player.game_ID).dayNightFreq)) < (Game.find(@player.game_ID).dayNightFreq*60))
+        details['time'] = 'day'
+      else
+        details['time'] = 'night'
+      end
+      details['place']=Player.order('score').all.index(Player.find_by_user_id(current_user.id))
+    else
+      details['isgame']= "no game"
+      details['status']="No Game Playing"
+      details['game_score']=""
+      details['alive']=""
+      details['werewolf']=0
+      details['townsperson']=0
+      details['time']="ended"
+      details['place']=""
+    end
+    respond_to do |format|
+      format.json { render json: details }
+    end
+
+  end
+
+  def totalscoreboard
+    score_hash = Hash.new
+    scores = User.where(:order => 'total_score', :limit => 5).all
+    i = 0
+    while i < 5
+      score_hash[i]=scores[i].email.split("@")[0] + ": " +scores[i].total_score.to_s
+      i+=1
+    end
+#
+    respond_to do |format|
+      format.json {render json: score_hash}
+    end
+
+  end
+  def gscoreboard
+    score_hash = Hash.new
+    if (!Player.first.nil?)
+      scores = Player.where(:order => 'score', :limit => 5).all
+      i = 0
+      while i < 5
+        score_hash[i]=scores[i].email.split("@")[0] + ": " + scores[i].high_score.to_s
+        i+=1
+      end
+    else
+      score_hash[0] = "No Current Game"
+      score_hash[1] = "N/A"
+      score_hash[2] = "N/A"
+      score_hash[3] = "N/A"
+      score_hash[4] = "N/A"
+    end
+
+
+    respond_to do |format|
+      format.json {render json: score_hash}
+    end
+
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,7 @@ require 'rufus-scheduler'
 
 
 class Game < ActiveRecord::Base
+
   attr_accessible :dayNightFreq, :dayORnight, :game_state, :kill_radius, :scent_radius
   validates :dayNightFreq, presence: true
   validates :game_state, presence: true
@@ -17,65 +18,28 @@ class Game < ActiveRecord::Base
 
   def set_roles
 
-    @cur_game = Game.last                                                         #set cur.game to the last game made
-    #puts @cur_game.dayNightFreq
+    @cur_game = Game.last
+
 
     create_players(@cur_game.id)
 
     size = Player.count
-    #puts size
+
     num_wolves = (size*3)/10
     i = 0
     while i < num_wolves do
 
-      @curPlayer = Player.offset(rand(Player.count)).first             #randomly selects 30% of players to
-      @curPlayer.Alignment = "werewolf"                                #be werewolfs
-      @curPlayer.save
+      @cur_player = Player.offset(rand(Player.count)).first
+      @cur_player.alignment = "werewolf"
+      @cur_player.save
       i+=1
     end
-
-    #@cur_time = @cur_game.dayORnight
-
-
-
-
     @scheduler = Rufus::Scheduler.start_new
-
-
-
-    @scheduler.every (Rufus.to_time_string(@cur_game.dayNightFreq*60)) do
-=begin
-      @cur_time = !@cur_time
-      if (@cur_time == true)
-        puts "it's nighttime"
-        @cur_game.update_attribute(:dayORnight, "true")                    #create new scheduler that
-
-      else                                                             #changes day/night based on the
-        @cur_game.update_attribute(:dayORnight, "false")                     #dayNightFreq (in minutes)
-        puts  "it's daytime"                                             #true = night, false = day
-
-      end
-      @cur_game.save
-=end
-    timer(@cur_game.id)
-
+    @scheduler.in (Rufus.to_time_string (@cur_game.dayNightFreq*60)) do
+      timer(@cur_game.id)
     end
-
-
-=begin
-    @players = Player.all                                                #sets all players to "townspeople"
-    for player in @players do
-      player.Alignment = "townspeople"
-      player.isDead = false
-      player.lat = rand(max = 100)
-      player.lng = rand(max = 100)
-      player.save
-    end
-=end
-
-
-
   end
+
 
   def timer(game_id)
     if Game.find(game_id).game_state != "ended"
@@ -95,52 +59,56 @@ class Game < ActiveRecord::Base
   def create_players(game_id)
     Player.delete_all
     User.all.each do |user|
-      #puts "making player"
-      @new_player = Player.new(:vote_cast => "false", :votes_for => 0, :game_id => game_id, :isDead => "false", :Alignment => "townsperson",
-                          :user_id => user.id, :score => 0, :lat => rand(10), :lng => rand(10), :nickname => user.email.split('@')[0])
-      @new_player.save
+      puts "creating player"
+      @new_p = Player.new(:kill_made => "false",:vote_cast => "false", :votes_for => 0, :game_ID => game_id, \
+                          :isDead => "false", :alignment => "townsperson", :user_id => user.id, :score => 0, \
+                          :lat => rand(10), :lng => rand(10), :nickname => user.email.split('@')[0])
+      @new_p.save
     end
   end
 
   def poll_votes(game_id)
-    most_votes = Player.first
 
+    puts "Polling Votes"
+    puts Time.now
+
+    @most_votes = Player.first
     Player.all.each do |player|
-      if player.votes_for > most_votes.votes_for
-        most_votes = player
+      if player.votes_for > @most_votes.votes_for
+        @most_votes = player
       end
-
-      most_votes.isDead = "true"
-      most_votes.save
+      @most_votes.isDead = "true"
+      @most_votes.save
     end
+
     Player.all.each do |player|
       if player.isDead == "false"
         player.score += 50
       end
+      player.kill_made = "false"
       player.votes_for = 0
       player.vote_cast = "false"
       player.save
     end
-    game_checker(game_id)
-
+    check_game(game_id)
 
   end
 
-  def game_checker(game_id)
-    @wolves = Player.where(:Alignment => "werewolf",:isDead => "false")
-    @towns = Player.where(:Alignment => "townsperson", :isDead => "false")
+  def check_game(game_id)
+    @wolves = Player.where(:alignment => "werewolf",:isDead => "false")
+    @townies = Player.where(:alignment => "townsperson", :isDead => "false")
     #puts @wolves
-    #puts @towns
-    if (@wolves.length > @towns.length) or (@wolves.length == 0)
+    #puts @townies
+    if (@wolves.length > @townies.length) or (@wolves.length == 0)
       @cur_game = Game.find(game_id)
       @cur_game.game_state = "ended"
       @cur_game.save
 
       @new_report = Report.new
-      if @wolves.length > @towns.length
+      if @wolves.length > @townies.length
         @new_report.winners = "Wolves"
         Player.all.each do |player|
-          if player.Alignment == "werewolf" and player.isDead == "false"
+          if player.alignment == "werewolf" and player.isDead == "false"
             player.score += 500
             player.save
           end
@@ -148,35 +116,33 @@ class Game < ActiveRecord::Base
       else
         @new_report.winners = "Townspeople"
         Player.all.each do |player|
-          if player.Alignment == "townsperson" and player.isDead == "false"
+          if player.alignment == "townsperson" and player.isDead == "false"
             player.score += 500
             player.save
           end
         end
       end
       @new_report.game_ID = @cur_game.id
-      high_score = Player.first
-
+      @high_score = Player.first
       Player.all.each do |player|
-        if player.score > high_score.score
-          high_score = player
+        if player.score > @high_score.score
+          @high_score = player
         end
       end
-      @new_report.high_score = high_score.nickname + " : " + high_score.score.to_s
+      @new_report.high_score = @high_score.nickname + " : " + @high_score.score.to_s
       @new_report.save
 
       Player.all.each do |player|
-        User.find(player.UserID).total_score += player.score
-        if player.score > User.find(player.UserID).high_score
-          User.find(player.UserID).high_score = player.score
+        User.find(player.user_id).total_score += player.score
+        if player.score > User.find(player.user_id).high_score
+          User.find(player.user_id).high_score = player.score
         end
-        User.find(player.UserID).save
+        User.find(player.user_id).save
       end
 
       Player.delete_all
     end
   end
-
 
 
 end
